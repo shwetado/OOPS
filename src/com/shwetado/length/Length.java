1bbd8fa803027eaee4d34ddd210d3d6bd1c755c1
Shweta <shwetadongare28@gmail.com>
2014-02-03 10:36:01 +0530

    extracted method getGreaterUnit()

--- src/com/shwetado/length/Length.java
+++ src/com/shwetado/length/Length.java
@@ -3,14 +3,14 @@ package com.shwetado.length;
 import com.shwetado.measurement.Measurement;
 import com.shwetado.measurement.Unit;
 
-public class Length extends Measurement{
+public class Length extends Measurement {
     public Length(double value, LengthUnit unit) {
-        super(value,unit);
+        super(value, unit);
     }
 
     @Override
     public boolean equals(Object o) {
-        if(super.equals(o))
+        if (super.equals(o))
             if ((o instanceof Length)) return true;
         return false;
     }
@@ -22,12 +22,16 @@ public class Length extends Measurement{
         return new Length(measurement.getValue(), (LengthUnit) measurement.getUnit());
     }
 
-    public Length add(Length length) {
-        LengthUnit unit = (LengthUnit) (this.getUnit().getUnitValue() > length.getUnit().getUnitValue()
-                ? this.getUnit() : length.getUnit());
+    public Length add(Length other) {
+        LengthUnit unit = getGreaterUnit((LengthUnit) other.getUnit());
         Length thisLength = this.convertTo(unit);
-        Length otherLength = length.convertTo(unit);
+        Length otherLength = other.convertTo(unit);
         double additionValue = thisLength.getValue() + otherLength.getValue();
         return new Length(additionValue, unit);
+    }
+
+    private LengthUnit getGreaterUnit(LengthUnit otherUnit) {
+        return (LengthUnit) (this.getUnit().getUnitValue() > otherUnit.getUnitValue()
+                ? this.getUnit() : otherUnit);
     }
 }
