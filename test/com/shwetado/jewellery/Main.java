788e2e0a86f01ad77d5dec3a7539689b463c1cd5
Shweta <shwetadongare28@gmail.com>
2014-02-03 16:52:35 +0530

    Main written which prints all artifacts

---
+++ test/com/shwetado/jewellery/Main.java
@@ -0,0 +1,16 @@
+package com.shwetado.jewellery;
+
+public class Main{
+    public static void main(String[] args) {
+        JewelleryBox jewelleryBox = new JewelleryBox("Blue Box");
+        Diamond diamond = new Diamond("Diamond");
+        jewelleryBox.addArtifact(diamond);
+        JewelleryBox smallBox = new JewelleryBox("Small Box");
+        Diamond diamond1 = new Diamond("Small Diamond");
+        jewelleryBox.addArtifact(smallBox);
+        jewelleryBox.addArtifact(diamond1);
+        Trinket trinket = new Trinket("Trinket");
+        jewelleryBox.addArtifact(trinket);
+        jewelleryBox.print();
+    }
+}
